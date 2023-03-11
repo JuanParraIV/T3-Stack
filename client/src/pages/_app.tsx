1d83ae67c50f0a4df1df5d3298ff6b10206ce06a
JuanParraIV <jmparra1993@gmail.com>
2023-03-11 10:10:55 -0500

    feat: configuring react-query

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,24 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from "@/QueryClient";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const MyApp: AppType<{ session: Session | null; }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   );
 };
 
